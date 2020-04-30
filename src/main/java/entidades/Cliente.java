f78f719cddcfa4a12733ba6d6de68bb309595809
Alvaro Ortiz <alvaro.ortizsanchez93@gmail.com>
2020-04-30 09:46:24 +0200

    Añadida información a la clase Cliente

--- src/main/java/entidades/Cliente.java
+++ src/main/java/entidades/Cliente.java
@@ -10,5 +10,136 @@ package entidades;
  * @author Alvaro
  */
 public class Cliente {
+    private Integer idCliente;
+    private String codigoCliente;
+    private String empresa;
+    private String contacto;
+    private String cargoContacto;
+    private String direccion;
+    private String ciudad;
+    private String region;
+    private String codigoPostal;
+    private String pais;
+    private String telefono;
+    private String fax;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer idCliente, String codigoCliente, String empresa, String contacto, String cargoContacto, String direccion, String ciudad, String region, String codigoPostal, String pais, String telefono, String fax) {
+        this.idCliente = idCliente;
+        this.codigoCliente = codigoCliente;
+        this.empresa = empresa;
+        this.contacto = contacto;
+        this.cargoContacto = cargoContacto;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.region = region;
+        this.codigoPostal = codigoPostal;
+        this.pais = pais;
+        this.telefono = telefono;
+        this.fax = fax;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(String codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getCargoContacto() {
+        return cargoContacto;
+    }
+
+    public void setCargoContacto(String cargoContacto) {
+        this.cargoContacto = cargoContacto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "idCliente=" + idCliente + ", codigoCliente=" + codigoCliente + ", empresa=" + empresa + ", contacto=" + contacto + ", cargoContacto=" + cargoContacto + ", direccion=" + direccion + ", ciudad=" + ciudad + ", region=" + region + ", codigoPostal=" + codigoPostal + ", pais=" + pais + ", telefono=" + telefono + ", fax=" + fax + '}';
+    }
     
 }
