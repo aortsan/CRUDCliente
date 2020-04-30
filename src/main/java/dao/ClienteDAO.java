bd82244e4720b7e4f846343a24cab5fd4eadc50b
Alvaro Ortiz <alvaro.ortizsanchez93@gmail.com>
2020-04-30 20:06:07 +0200

    Añadida más información a la clase ClienteDAO y menú en el Main

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -12,8 +12,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -23,6 +21,9 @@ public class ClienteDAO {
 
     private Connection conexion = null;
 
+    /**
+     * Establecer conexión con la base de datos
+     */
     public ClienteDAO() {
         try {
             conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/neptuno", "root", "");
@@ -35,24 +36,32 @@ public class ClienteDAO {
         return conexion;
     }
 
-    public ArrayList<Cliente> listar(String orden, Integer desde, Integer limite) {
+    /**
+     * Listar la base de datos mediante los siguientes parámetros:
+     *
+     * @param orden
+     * @param inicio
+     * @param limite
+     * @return
+     */
+    public ArrayList<Cliente> listar(String orden, Integer inicio, Integer limite) {
         ArrayList<Cliente> listaClientes = new ArrayList<>();
         PreparedStatement stm = null;
-        
-        if(this.conexion == null || orden == null || desde == null || limite == null){
+
+        if (this.conexion == null || orden == null || inicio == null || limite == null) {
             return listaClientes;
         }
-        
+
         try {
             String query = "SELECT * FROM clientes ORDER BY " + orden + " LIMIT ? OFFSET ?";
-            
+
             stm = conexion.prepareStatement(query);
-            stm.setInt(1,limite);
-            stm.setInt(2, desde);
-            
+            stm.setInt(1, limite);
+            stm.setInt(2, inicio);
+
             ResultSet rs = stm.executeQuery();
-            
-            while (rs.next()){
+
+            while (rs.next()) {
                 Cliente cliente = new Cliente(
                         rs.getInt("id"),
                         rs.getString("codigo"),
@@ -69,17 +78,152 @@ public class ClienteDAO {
                 );
                 listaClientes.add(cliente);
             }
-            
+
         } catch (SQLException ex) {
             System.err.println("Error al listar: " + ex.getMessage() + " " + stm.toString());
-        } finally{
+        } finally {
             try {
-                stm.close();
+                if (stm != null) {
+                    stm.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println("Error al cerrar la conexión: " + ex.getMessage());
+            }
+        }
+
+        return listaClientes;
+    }
+
+    /**
+     * Insertar un cliente
+     *
+     * @param cliente
+     * @return
+     */
+    public Boolean insertar(Cliente cliente) {
+        Boolean resultado = false;
+        PreparedStatement stm = null;
+
+        if (this.conexion == null) {
+            return resultado;
+        }
+
+        try {
+            String query = "INSERT INTO clientes "
+                    + "(id, codigo, empresa, contacto, cargo_contacto, direccion, ciudad, region, cp, pais, telefono, fax) "
+                    + "VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            stm = conexion.prepareStatement(query);
+            stm.setString(1, cliente.getCodigoCliente());
+            stm.setString(2, cliente.getEmpresa());
+            stm.setString(3, cliente.getContacto());
+            stm.setString(4, cliente.getCargoContacto());
+            stm.setString(5, cliente.getDireccion());
+            stm.setString(6, cliente.getCiudad());
+            stm.setString(7, cliente.getRegion());
+            stm.setString(8, cliente.getCodigoPostal());
+            stm.setString(9, cliente.getPais());
+            stm.setString(10, cliente.getTelefono());
+            stm.setString(11, cliente.getTelefono());
+
+            if (stm.executeUpdate() > 0) {
+                resultado = true;
+
+            }
+
+        } catch (SQLException ex) {
+            System.err.println("Error al insertar: " + ex.getMessage());
+        } finally {
+            try {
+                if (stm != null) {
+                    stm.close();
+                }
             } catch (SQLException ex) {
                 System.err.println("Error al cerrar la conexión: " + ex.getMessage() + " " + stm.toString());
             }
         }
-        
-        return listaClientes;
+
+        return resultado;
+    }
+
+    /**
+     * Actualización del cliente a través de un campo que se le pedirá al
+     * usuario
+     *
+     * @param id
+     * @param campo
+     * @return
+     */
+    public Boolean update(Integer id, String campo) {
+        Boolean resultado = null;
+        PreparedStatement stmt = null;
+
+        if (this.conexion == null || id == null || campo == null) {
+            return resultado;
+        }
+
+        try {
+
+            String sql = "UPDATE empleados SET " + campo + "=? WHERE codigoempleado = ?";
+
+            stmt = conexion.prepareStatement(sql);
+            stmt.setString(1, campo);
+            stmt.setInt(2, id);
+
+            if (stmt.executeUpdate() > 0) {
+                resultado = true;
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Error en el Update: " + e.getMessage());
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println("Error al cerrar la conexin: " + ex.getMessage());
+            }
+        }
+
+        return resultado;
+    }
+
+    /**
+     * Eliminar un registro a través del id del cliente
+     *
+     * @param idCliente
+     * @return
+     */
+    public Boolean delete(Integer idCliente) {
+        Boolean resultado = false;
+        PreparedStatement stmt = null;
+
+        if (this.conexion == null || idCliente == null) {
+            return resultado;
+        }
+
+        try {
+            String sql = "DELETE FROM empleados WHERE codigoempleado = ?";
+            stmt = conexion.prepareStatement(sql);
+            stmt.setInt(1, idCliente);
+
+            resultado = stmt.execute();
+
+        } catch (SQLException e) {
+
+            System.err.println("Error en el Delete: " + e.getMessage());
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println("Error al cerrar la conexin: " + ex.getMessage());
+            }
+        }
+
+        return resultado;
+
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,11 @@
  */
 package main;
 
+import dao.ClienteDAO;
+import entidades.Cliente;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Alvaro
@@ -14,8 +19,72 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    static ArrayList<Cliente> listaClientes = null;
+    static ClienteDAO clientes = new ClienteDAO();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in, "ISO-8859-1");
+        Integer opcion;
+        Integer inicio = 0;
+        Integer limite = 10;
+
+        if (clientes.getConexion() == null) {
+            System.err.println("Programa terminado. Error en la conexión.");
+            System.exit(0);
+        }
+
+        System.out.println("\t\tBIENVENIDO");
+        System.out.println("\t\t-----------");
+        while (true) {
+            try {
+                System.out.println("ELIJA ALGUNA DE LAS OPCIONES QUE SE MUESTRAN A CONTINUACIÓN\n");
+                System.out.println("  1. Visualizar los diez siguientes.");
+                System.out.println("  2. Visualizar los diez siguientes.");
+                System.out.println("  3. Introducir nuevo registro.");
+                System.out.println("  4. Actualizar dato de cliente por IdCliente.");
+                System.out.println("  5. Borrar dato por IdCliente.\n");
+                System.out.print("Su elección [introduzca 0 para salir]: ");
+                opcion = Integer.parseInt(sc.nextLine());
+
+                switch (opcion) {
+                    case 0:
+                        System.out.println("\nHasta pronto.\n");
+                        System.out.println("\t    -------------");
+                        System.out.println("\t\tFIN\n");
+                        System.exit(0);
+                        break;
+                    case 1:
+                        listarClientes(inicio,limite);
+                        break;
+                    case 2:
+                        listarClientes(inicio,limite);
+                        break;
+                    case 3:
+                        System.out.println("\nINSERTAR");
+                        System.out.println("--------");
+                        break;
+                    case 4:
+                        System.out.println("\nACTUALIZAR");
+                        System.out.println("----------");
+                        break;
+                    case 5:
+                        System.out.println("\nBORRAR");
+                        System.out.println("------");
+                }
+                System.out.println();
+            } catch (NumberFormatException nfe) {
+                System.err.println("\nError: Entrada no válida." + nfe.getMessage() + "\n");
+            }
+        }
     }
-    
+
+    public static void listarClientes(Integer inicio, Integer limite) {
+        System.out.println("\nLISTADO");
+        System.out.println("--------");
+        listaClientes = clientes.listar("codigo", inicio, limite);
+        for (Cliente c : listaClientes) {
+            System.out.println(c);
+        }
+    }
+
 }
