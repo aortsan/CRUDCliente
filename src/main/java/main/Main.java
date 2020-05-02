d71ad4dac4c4c73784fe71ec8ac51d662718688e
Alvaro Ortiz <alvaro.ortizsanchez93@gmail.com>
2020-05-02 22:05:08 +0200

    Añadido menú para listar según el campo indicado

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
     static ClienteDAO clientes = new ClienteDAO();
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in, "ISO-8859-1");
+        Scanner sc = new Scanner(System.in);
         Integer opcion;
         Integer inicio = 0;
         Integer limite = 10;
@@ -54,10 +54,10 @@ public class Main {
                         System.exit(0);
                         break;
                     case 1:
-                        listarClientes(inicio,limite);
+                        listarClientes();
                         break;
                     case 2:
-                        listarClientes(inicio,limite);
+                        listarClientes();
                         break;
                     case 3:
                         System.out.println("\nINSERTAR");
@@ -73,17 +73,138 @@ public class Main {
                 }
                 System.out.println();
             } catch (NumberFormatException nfe) {
-                System.err.println("\nError: Entrada no válida." + nfe.getMessage() + "\n");
+                System.err.println("\nError: Entrada no válida. " + nfe.getMessage() + "\n");
             }
         }
     }
 
-    public static void listarClientes(Integer inicio, Integer limite) {
+    public static void listarClientes() {
+        Scanner sc = new Scanner(System.in);
+        Integer opcion;
+
+        System.out.println("\nIndique según que orden quiere listar: \n");
+        System.out.println("  1.  Id del cliente\n"
+                + "  2.  Código del cliente\n"
+                + "  3.  Empresa\n"
+                + "  4.  Contacto\n"
+                + "  5.  Cargo del contacto\n"
+                + "  6.  Dirección\n"
+                + "  7.  Ciudad\n"
+                + "  8.  Región\n"
+                + "  9.  Código postal\n"
+                + "  10. País\n"
+                + "  11. Teléfono\n"
+                + "  12. Fax");
+        System.out.print("\nSu opción: ");
+        opcion = Integer.parseInt(sc.nextLine());
+        System.out.println();
+        switch (opcion) {
+            case 1:
+                listado("id",0,10);
+                break;
+            case 2:
+                listado("codigo",0,10);
+                break;
+            case 3:
+                listado("empresa",0,10);
+                break;
+            case 4:
+                listado("contacto",0,10);
+                break;
+            case 5:
+                listado("cargo_contacto",0,10);
+                break;
+            case 6:
+                listado("direccion",0,10);
+                break;
+            case 7:
+                listado("ciudad",0,10);
+                break;
+            case 8:
+                listado("region",0,10);
+                break;
+            case 9:
+                listado("cp",0,10);
+                break;
+            case 10:
+                listado("pais",0,10);
+                break;
+            case 11:
+                listado("telefono",0,10);
+                break;
+            case 12:
+                listado("fax",0,10);
+                break;
+        }
+    }
+
+    public static void listado(String orden, Integer inicio, Integer limite) {
         System.out.println("\nLISTADO");
         System.out.println("--------");
-        listaClientes = clientes.listar("codigo", inicio, limite);
+        System.out.printf("|%-10s| %-40s| %-20s| %-30s|%n","Id","Empresa","Contacto","Cargo");
+        listaClientes = clientes.listar(orden, inicio, limite);
         for (Cliente c : listaClientes) {
-            System.out.println(c);
+            System.out.printf("|%-10d| %-40s| %-20s| %-30s|%n",c.getIdCliente(),c.getEmpresa(),c.getContacto(),c.getCargoContacto());
+        }
+    }
+    
+    public static void actualizar() {
+        Scanner sc = new Scanner(System.in);
+        Integer opcion;
+
+        System.out.println("\nIndique el campo que desea modificar: \n");
+        System.out.println("  1.  Id del cliente\n"
+                + "  2.  Código del cliente\n"
+                + "  3.  Empresa\n"
+                + "  4.  Contacto\n"
+                + "  5.  Cargo del contacto\n"
+                + "  6.  Dirección\n"
+                + "  7.  Ciudad\n"
+                + "  8.  Región\n"
+                + "  9.  Código postal\n"
+                + "  10. País\n"
+                + "  11. Teléfono\n"
+                + "  12. Fax");
+        System.out.print("\nSu opción: ");
+        opcion = Integer.parseInt(sc.nextLine());
+        System.out.println();
+        switch (opcion) {
+            case 1:
+                listado("id",0,10);
+                break;
+            case 2:
+                listado("codigo",0,10);
+                break;
+            case 3:
+                listado("empresa",0,10);
+                break;
+            case 4:
+                listado("contacto",0,10);
+                break;
+            case 5:
+                listado("cargo_contacto",0,10);
+                break;
+            case 6:
+                listado("direccion",0,10);
+                break;
+            case 7:
+                listado("ciudad",0,10);
+                break;
+            case 8:
+                listado("region",0,10);
+                break;
+            case 9:
+                listado("cp",0,10);
+                break;
+            case 10:
+                listado("pais",0,10);
+                break;
+            case 11:
+                listado("telefono",0,10);
+                break;
+            case 12:
+                listado("fax",0,10);
+                break;
         }
     }
 
